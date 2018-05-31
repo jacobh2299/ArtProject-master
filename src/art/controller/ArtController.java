7303e9f772b4f7be1b7e453891aeb9d4fb05ee6b
jacobh2299 <31513783+jacobh2299@users.noreply.github.com>
2018-05-31 09:41:10 -0600

    fixed up ShapeCanvas

--- src/art/controller/ArtController.java
+++ src/art/controller/ArtController.java
@@ -1,9 +1,22 @@
 package art.controller;
 
+import java.awt.Component;
+import java.io.IOException;
+
 public class ArtController 
 {
 	public void start()
 	{
+		
+	}
+
+	public Component getFrame() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void handleErrors(IOException error) {
+		// TODO Auto-generated method stub
 		
 	}
 }
